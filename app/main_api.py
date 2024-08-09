f60494be0b77994bb277e329a26e3d951862fb06
Devnuvols <miguel.monllau.monfort@gmail.com>
2024-08-09 14:05:34 +0200

    bugs instal

--- app/main_api.py
+++ app/main_api.py
@@ -17,7 +17,7 @@ from jwt.exceptions import PyJWTError
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.auth_whats import check_user_exists, get_chat_id,  insert_QA, update_feedback
-from utils.llm_bot import LLM_chat, get_context, get_messages_from_chat_id, respuesta_emoji, UserData
+from utils.llm_bot import LLM_chat, get_context, get_messages_from_chat_id, respuesta_emoji
 
 
 from Langdetect import detect
